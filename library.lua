b5369d46914e9175b926ab706cf6b2c398353360
ighhuiktyfgujehbfy <132170543+ighhuiktyfgujehbfy@users.noreply.github.com>
2023-05-02 23:35:57 +0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -3631,7 +3631,7 @@ function library.new(options)
 						Name = "game"
 					}),
 					create("ImageLabel", {
-						Image = "",
+						Image = "http://www.roblox.com/asset/?id=8569322835",
 						BackgroundColor3 = Color3.new(1, 1, 1),
 						BackgroundTransparency = 1,
 						Position = UDim2.new(0, 8, 0, 16),
